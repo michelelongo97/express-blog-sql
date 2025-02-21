77c54e47f5a37d6b45245360d29678f6f4245903
michelelongo97 <m.longo7x@gmail.com>
2025-02-21 12:54:59 +0100

    feat: add milestone 2

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -4,7 +4,17 @@ const postsData = require("../data/posts");
 
 //index
 const index = (req, res) => {
-  res.json(postsData);
+  const sql = `SELECT * FROM posts`;
+
+  connection.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Database query failed",
+      });
+    }
+
+    res.json(results);
+  });
 };
 
 //show
